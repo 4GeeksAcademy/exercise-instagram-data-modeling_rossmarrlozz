4d3dea5aa9f92fa1962f204d28b9109a445b53b1
rossmarrlozz <168719962+rossmarrlozz@users.noreply.github.com>
2024-09-15 21:00:26 +0000

    hecho

--- src/models.py
+++ src/models.py
@@ -1,34 +1,103 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, create_engine
 from sqlalchemy.orm import relationship, declarative_base
-from sqlalchemy import create_engine
 from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+Base = declarative_base()
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class User(Base):
+    __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    username = Column(String, nullable=False)
+    firstname = Column(String)
+    lastname = Column(String)
+    email = Column(String, nullable=False)
+
+    posts = relationship('Post', back_populates='user')
+    comments = relationship('Comment', back_populates='author')
 
     def to_dict(self):
-        return {}
+        return {
+            'id': self.id,
+            'username': self.username,
+            'firstname': self.firstname,
+            'lastname': self.lastname,
+            'email': self.email,
+            'posts': [post.to_dict() for post in self.posts],
+            'comments': [comment.to_dict() for comment in self.comments]
+        }
+
+class Follower(Base):
+    __tablename__ = 'follower'
+    user_from_id = Column(Integer, ForeignKey('user.id'), primary_key=True)
+    user_to_id = Column(Integer, ForeignKey('user.id'), primary_key=True)
+
+    user_from = relationship('User', foreign_keys=[user_from_id])
+    user_to = relationship('User', foreign_keys=[user_to_id])
+
+    def to_dict(self):
+        return {
+            'user_from_id': self.user_from_id,
+            'user_to_id': self.user_to_id
+        }
+
+class Post(Base):
+    __tablename__ = 'post'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'), nullable=False)
+
+    user = relationship('User', back_populates='posts')
+    comments = relationship('Comment', back_populates='post')
+    media = relationship('Media', back_populates='post')
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'user_id': self.user_id,
+            'comments': [comment.to_dict() for comment in self.comments],
+            'media': [m.to_dict() for m in self.media]
+        }
+
+class Comment(Base):
+    __tablename__ = 'comment'
+    id = Column(Integer, primary_key=True)
+    comment_text = Column(String, nullable=False)
+    author_id = Column(Integer, ForeignKey('user.id'), nullable=False)
+    post_id = Column(Integer, ForeignKey('post.id'), nullable=False)
+
+    author = relationship('User', back_populates='comments')
+    post = relationship('Post', back_populates='comments')
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'comment_text': self.comment_text,
+            'author_id': self.author_id,
+            'post_id': self.post_id
+        }
+
+class Media(Base):
+    __tablename__ = 'media'
+    id = Column(Integer, primary_key=True)
+    type = Column(String, nullable=False)
+    url = Column(String, nullable=False)
+    post_id = Column(Integer, ForeignKey('post.id'), nullable=False)
+
+    post = relationship('Post', back_populates='media')
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'type': self.type,
+            'url': self.url,
+            'post_id': self.post_id
+        }
 
 ## Draw from SQLAlchemy base
+
 try:
     result = render_er(Base, 'diagram.png')
     print("Success! Check the diagram.png file")
